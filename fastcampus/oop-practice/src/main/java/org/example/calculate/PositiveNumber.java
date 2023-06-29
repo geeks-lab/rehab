8d35f68d21fac88e3bba39806ba405c8a1f9897e
gigis-lab <churn82@gmail.com>
2023-06-29 15:04:53 +0900

    사칙연산 계산기 객체지향으로 만들기

---
+++ fastcampus/oop-practice/src/main/java/org/example/calculate/PositiveNumber.java
@@ -0,0 +1,27 @@
+package org.example.calculate;
+
+
+//양수 validation을 위한 value object 생성
+public class PositiveNumber {
+    private final int value;
+
+    public PositiveNumber(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        //value값이 음수가 맞다면 예외를 만든다.
+        if (isNegativeNumber(value)) {
+            throw new IllegalArgumentException("0또는 음수를 전달할 수 없습니다.");
+        }
+    }
+
+    private boolean isNegativeNumber(int value) {
+        return value <= 0;
+    }
+
+    public int toInt() {
+        return value;
+    }
+}
