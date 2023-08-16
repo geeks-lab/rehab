690ba27879314684d9c8e900ec2dd162330e2ad6
gigis-lab <churn82@gmail.com>
2023-08-16 23:53:23 +0900

    findConstructor 코드 완성

--- fastcampus/di-practice/src/main/java/org/example/di/BeanFactory.java
+++ fastcampus/di-practice/src/main/java/org/example/di/BeanFactory.java
@@ -1,10 +1,12 @@
 package org.example.di;
 
+import org.example.annotation.Inject;
 import org.example.controller.UserController;
+import org.reflections.ReflectionUtils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class BeanFactory {
     private final Set<Class<?>> preInstantiatedClazz;
@@ -12,8 +14,47 @@ public class BeanFactory {
 
     public BeanFactory(Set<Class<?>> preInstantiatedClazz) {
         this.preInstantiatedClazz = preInstantiatedClazz;
+        initialize();
     }
-    // 인자로 넘어온 클래스타입 객체를 키로 전달해서 인스턴스를 반환한다.
+
+    private void initialize() {
+        for (Class<?> clazz : preInstantiatedClazz) {
+            Object instance = createInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    private Object createInstance(Class<?> clazz) {
+        // 인스턴스를 생성하기 위해 생성자가 필요하다
+        Constructor<?> constructor = findConstructor(clazz);
+
+        // 생성자에 어떤 파라미터가 들어가는지 파리미터 정보가 필요하다
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes()) {
+            parameters.add(getParameterByClass(typeClass));
+        }
+
+        // 인스턴스 생성
+        try {
+            return constructor.newInstance(parameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz){
+        Set<Constructor> injectedConstructors = ReflectionUtils.getAllConstructors(clazz, ReflectionUtils.withAnnotation(Inject.class));
+        if(injectedConstructors.isEmpty()){
+            return null;
+        }
+        return injectedConstructors.iterator().next();
+
+    }
+
+
+    // 자로 넘어온 클래스타입 객체를 키로 전달해서 인스턴스를 반환한다.
     public <T> T getBean(Class<T> requiredType) {
         return (T) beans.get(requiredType);
     }
